9b1a3849d7bf5e02dd098cc816e841f8992ce0a3
toass <taoss3932@gmail.com>
2023-03-23 03:17:01 +0900

    calendar checkpoint 2 --adapter&view finished

--- app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
+++ app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
@@ -1,35 +1,24 @@
 package com.skku.cmdapp.couplecalendar
 
-import DayViewContainer
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
-import com.kizitonwose.calendar.view.CalendarView
-import com.kizitonwose.calendar.view.MonthDayBinder
-import java.time.YearMonth
+import com.skku.cmdapp.couplecalendar.databinding.ActivityMainBinding
+import com.skku.cmdapp.couplecalendar.view.CalendarViewActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var calendarView: CalendarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        calendarView = findViewById(R.id.calendarView)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleClick(binding)
+    }
 
-        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            override fun create(view: View) = DayViewContainer(view)
-
-            override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.textView.text = data.date.dayOfMonth.toString()
-            }
+    private fun handleClick(binding: ActivityMainBinding){
+        binding.openCalendarBtn.setOnClickListener{
+            startActivity(Intent(this, CalendarViewActivity::class.java))
         }
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
-        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
-        calendarView.setup(startMonth, endMonth, firstDayOfWeek)
-        calendarView.scrollToMonth(currentMonth)
     }
 }
