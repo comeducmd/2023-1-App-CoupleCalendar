0d3b65e68ead7a910b501c712575f47ffecb48ea
toass <taoss3932@gmail.com>
2023-03-22 19:43:44 +0900

    calendar checkpoint 1

--- app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
+++ app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.skku.cmdapp.couplecalendar
 
+import DayViewContainer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.view.CalendarView
+import com.kizitonwose.calendar.view.MonthDayBinder
+import java.time.YearMonth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var calendarView: CalendarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calendarView = findViewById(R.id.calendarView)
+
+        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                container.textView.text = data.date.dayOfMonth.toString()
+            }
+        }
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
+        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
+        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
+        calendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        calendarView.scrollToMonth(currentMonth)
     }
 }
