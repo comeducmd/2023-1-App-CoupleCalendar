223273f91e7d4d7b5b2f5209511a850750f5d41a
JJaeseo <altanev@naver.com>
2023-03-29 16:33:18 +0900

    popup practice
    popupdialog.kt (버튼 누르면 팝업 되게 만드는 함수 설정) 새로 만들었고 MainActivity에도 diarybuttonclicked 함수를 만들었습니당

--- app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
+++ app/src/main/java/com/skku/cmdapp/couplecalendar/MainActivity.kt
@@ -3,8 +3,10 @@ package com.skku.cmdapp.couplecalendar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.skku.cmdapp.couplecalendar.databinding.ActivityMainBinding
 import com.skku.cmdapp.couplecalendar.view.CalendarViewActivity
+import com.skku.cmdapp.couplecalendar.view.popupdialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,4 +23,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, CalendarViewActivity::class.java))
         }
     }
+
+    fun diarybuttonclicked(view: View) {
+
+        val diarypopup = popupdialog(this)
+
+        diarypopup.show()
+    }
+
 }
